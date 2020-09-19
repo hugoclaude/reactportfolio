fc0245e8034aff5ad96bcca149f619a5688b28d9
hugoclaude <hugocayeros@gmail.com>
2020-09-18 18:58:35 -0700

    react update

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import moment from "moment";
 
+import PortfolioContainer from './portfolio-container'
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Hugo Cayeros Portfolio</h1>
+        <PortfolioContainer />
     <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
       </div>
     );
---
+++ src/components/portfolio-container.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class App extends Componenet {
+    render() {
+        return (
+            <div>
+              <h2>Portfolio items go here...</h2>
+            </div>
+        )
+    }
+}
