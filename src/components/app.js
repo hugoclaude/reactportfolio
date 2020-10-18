a93872e3ff1c7b7ddd3cb505c5c7ed22ae4f617e
hugoclaude <hugocayeros@gmail.com>
2020-10-18 13:01:28 -0700

    commit update

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,6 @@
-  
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import axios from 'axios';
 
 import NavigationContainer from "./navigation/navigation-container";
 import Home from "./pages/home";
@@ -35,12 +35,45 @@ export default class App extends Component {
     });
   }
 
+  checkLoginStatus() {
+    return axios.get("https://api.devcamp.space/logged_in", {
+      withCredentials: true
+    })
+  .then( response => {
+    const loggedIn = response.data.logged_in;
+    const loggedInStatus = this.state.loggedInStatus;
+
+    // If loggedIn and status LOGGED_IN => return data
+    // If loggedIn status NOT_LOGGED_IN => update status
+    //If not loggedIn and status LOGGED_IN => update state
+
+    if (loggedIn && loggedInStatus === "LOGGED_IN") {
+      return loggedIn;
+    } else if (loggedIn && loggedInStatus === "NOT_LOGGED_IN") {
+      this.setState({
+        loggedInStatus: "LOGGED_IN"
+      });
+    } else if (!loggedIn && loggedInStatus === "NOT_LOGGED_IN") {
+      this.setState({
+        loggedInStatus: "LOGGED_IN"
+      });
+    }
+  })
+  .catch(error => {
+    console.log("Error", error);
+  });
+}
+
+componentDidMount() {
+  this.checkLoginStatus();
+}
+
   render() {
     return (
       <div className="container">
         <Router>
           <div>
-            <NavigationContainer />
+            <NavigationContainer  loggedInStatus={this.state.loggedInStatus}/>
 
             <h2>{this.state.loggedInStatus}</h2>
 
--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,42 +1,44 @@
-import React, { Component } from 'react';
+import React from "react";
 import { NavLink } from 'react-router-dom';
 
-export default class NavigationComponent extends Component {
-    constructor() {
-        super();
-    }
+const NavigationComponent = (props) => {
+  const dynamicLink = (route, linkText) => {
+    return(
+      <div className="nav-link-wrapper">
+      <NavLink to="/blog" activeClassName="nav-link-active">
+        Blog
+      </NavLink>
+    </div>
+    );
+  };
 
-    render() {
-        return (
-          <div className="nav-wrapper">
-            <div className="left-side">
-              <div className="nav-link-wrapper">
-                <NavLink exact to="/" activeClassName="nav-link-active">
-                  Home
-                </NavLink>
-              </div>
-    
-              <div className="nav-link-wrapper">
-                <NavLink to="/about-me" activeClassName="nav-link-active">
-                  About
-                </NavLink>
-              </div>
-    
-              <div className="nav-link-wrapper">
-                <NavLink to="/contact" activeClassName="nav-link-active">
-                  Contact
-                </NavLink>
-              </div>
-    
-              <div className="nav-link-wrapper">
-                <NavLink to="/blog" activeClassName="nav-link-active">
-                  Blog
-                </NavLink>
-              </div>
-            </div>
-    
-            <div className="right-side">Hugo Cayeros</div>
-          </div>
-        );
-      }
-    }
+  return (
+    <div className="nav-wrapper">
+      <div className="left-side">
+        <div className="nav-link-wrapper">
+          <NavLink exact to="/" activeClassName="nav-link-active">
+            Home
+          </NavLink>
+        </div>
+
+        <div className="nav-link-wrapper">
+          <NavLink to="/about-me" activeClassName="nav-link-active">
+            About
+          </NavLink>
+        </div>
+
+        <div className="nav-link-wrapper">
+          <NavLink to="/contact" activeClassName="nav-link-active">
+            Contact
+          </NavLink>
+        </div>
+
+        {props.loggedInStatus === "LOGGED_IN" ? dynamicLink("/blog", "Blog") : null}
+      </div>
+
+      <div className="right-side">Hugo Cayeros</div>
+    </div>
+  );
+}
+
+export default NavigationComponent;
