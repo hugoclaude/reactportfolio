c7ca3a06a8ef4a7c5a4ba842d919a530ee76fd79
hugoclaude <hugocayeros@gmail.com>
2020-11-02 16:15:59 -0800

    blog styles added

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -35,7 +35,13 @@ class Blog extends Component {
         return <BlogItem key={blogItem.id} blogItem={blogItem} />
       });
 
-    return (<div>{blogRecords}</div>);
+    return (
+      <div className="blog-container">
+      <div className="content-container">
+      {blogRecords}
+      </div>
+      </div>
+    );
   }
 }
 
