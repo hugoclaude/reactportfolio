621c2286b4c7865e59d875fc73c93efc16cda8df
hugoclaude <hugocayeros@gmail.com>
2020-11-04 15:57:59 -0800

    infinite scrolling mods

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import axios from 'axios';
+import axios from "axios";
 import BlogItem from "../blog/blog-item";
 
 class Blog extends Component {
   constructor() {
-    super ();
+    super();
 
     this.state = {
       blogItems: [],
@@ -22,10 +22,17 @@ class Blog extends Component {
   activateInfiniteScroll() {
     window.onscroll = () => {
       if (
+        this.state.isLoading ||
+        this.state.blogItems.length === this.state.totalCount
+      ) {
+        return;
+      }
+
+      if (
         window.innerHeight + document.documentElement.scrollTop ===
         document.documentElement.offsetHeight
       ) {
-        console.log("get more posts");
+        this.getBlogItems();
       }
     };
   }
@@ -34,36 +41,46 @@ class Blog extends Component {
     this.setState({
       currentPage: this.state.currentPage + 1
     });
-    axios.get("https://allisdust.devcamp.space/portfolio/portfolio_blogs", {withCredentials: true
-    }).then(response => {
-      this.setState({
-        blogItems: response.data.portfolio_blogs,
-        totalCount: response.data.meta.total_records,
-        isLoading: false
+
+    axios
+      .get(
+        `https://allisdust.devcamp.space/portfolio/portfolio_blogs?page=${this
+          .state.currentPage}`,
+        {
+          withCredentials: true
+        }
+      )
+      .then(response => {
+        console.log("gettting", response.data);
+        this.setState({
+          blogItems: this.state.blogItems.concat(response.data.portfolio_blogs),
+          totalCount: response.data.meta.total_records,
+          isLoading: false
+        });
+      })
+      .catch(error => {
+        console.log("getBlogItems error", error);
       });
-    })
-    .catch(error => {
-      console.log("getBlogItems error", error);
-    });
   }
 
   componentWillMount() {
     this.getBlogItems();
   }
 
-    render() {
-      const blogRecords = this.state.blogItems.map(blogItem => {
-        return <BlogItem key={blogItem.id} blogItem={blogItem} />
-      });
+  render() {
+    const blogRecords = this.state.blogItems.map(blogItem => {
+      return <BlogItem key={blogItem.id} blogItem={blogItem} />;
+    });
 
     return (
       <div className="blog-container">
         <div className="content-container">{blogRecords}</div>
 
         {this.state.isLoading ? (
-        <div className="content-loader">
-          <FontAwesomeIcon icon="spinner" spin />
-        </div>) : null}
+          <div className="content-loader">
+            <FontAwesomeIcon icon="spinner" spin />
+          </div>
+        ) : null}
       </div>
     );
   }
