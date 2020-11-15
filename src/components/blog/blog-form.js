9184348b794680071ea4b14d15c74b6b3e628d41
hugoclaude <hugocayeros@gmail.com>
2020-11-15 08:16:18 -0800

    blog form updated

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -39,17 +39,19 @@ export default class BlogForm extends Component {
       .post(
         "https://allisdust.devcamp.space/portfolio/portfolio_blogs",
         this.buildForm(),
+
         { withCredentials: true }
       )
       .then((response) => {
-        this.props.handleSuccessfullFormSubmission(
-          response.data.portfolio_blog
-        );
-
         this.setState({
           title: "",
           blog_status: "",
         });
+        
+        this.props.handleSuccessfullFormSubmission(
+          response.data.portfolio_blog
+        );
+
       })
       .catch((error) => {
         console.log("handleSubmit for blog error", error);
