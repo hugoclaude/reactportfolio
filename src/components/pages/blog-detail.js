d83a5085fd2571634135a547c0e8f96609b77db0
hugoclaude <hugocayeros@gmail.com>
2020-11-20 07:49:10 -0800

    new commit

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -16,11 +16,31 @@ export default class BlogDetail extends Component {
     };
 
     this.handleEditClick = this.handleEditClick.bind(this);
+    this.handleFeaturedImageDelete = this.handleFeaturedImageDelete.bind(this);
+    this.handleUpdateFormSubmission = this.handleUpdateFormSubmission.bind(
+      this
+    );
+  }
+
+  handleUpdateFormSubmission(blog) {
+    this.setState({
+      blogItem: blog,
+      editMode: false,
+    });
+  }
+
+  handleFeaturedImageDelete() {
+    this.setState({
+      blogItem: {
+        featured_image_url: "",
+      },
+    });
   }
 
   handleEditClick() {
-    console.log("handle edit clicked");
+    if (this.props.loggedInStatus === "LOGGED_IN") {
     this.setState({ editMode: true });
+    }
   }
 
   getBlogItem() {
@@ -53,7 +73,12 @@ export default class BlogDetail extends Component {
     const contentManager = () => {
       if (this.state.editMode) {
         return (
-          <BlogForm editMode={this.state.editMode} blog={this.state.blogItem} />
+          <BlogForm
+            handleFeaturedImageDelete={this.handleFeaturedImageDelete}
+            handleUpdateFormSubmission={this.handleUpdateFormSubmission}
+            editMode={this.state.editMode}
+            blog={this.state.blogItem}
+          />
         );
       } else {
         return (
