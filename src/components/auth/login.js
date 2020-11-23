2d9efbae4dea57a72b5fb739b2f676f24ad199bb
hugoclaude <hugocayeros@gmail.com>
2020-11-23 12:27:21 -0800

    login styles applied

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default class Login extends Component {
   constructor(props) {
@@ -8,7 +9,7 @@ export default class Login extends Component {
     this.state = {
       email: "",
       password: "",
-      errorText: ""
+      errorText: "",
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -18,7 +19,7 @@ export default class Login extends Component {
   handleChange(event) {
     this.setState({
       [event.target.name]: event.target.value,
-      errorText: ""
+      errorText: "",
     });
   }
 
@@ -29,26 +30,26 @@ export default class Login extends Component {
         {
           client: {
             email: this.state.email,
-            password: this.state.password
-          }
+            password: this.state.password,
+          },
         },
         { withCredentials: true }
       )
-      .then(response => {
+      .then((response) => {
         if (response.data.status === "created") {
           console.log("You can come in...");
-          console.log("testing testing")
+          console.log("testing testing");
           this.props.handleSuccessfulAuth();
         } else {
           this.setState({
-            errorText: "Wrong email or password"
+            errorText: "Wrong email or password",
           });
           this.props.handleUnsuccessfulAuth();
         }
       })
-      .catch(error => {
+      .catch((error) => {
         this.setState({
-          errorText: "An error occurred"
+          errorText: "An error occurred",
         });
         this.props.handleUnsuccessfulAuth();
       });
@@ -61,28 +62,34 @@ export default class Login extends Component {
       <div>
         <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
 
-        <div>{this.state.errorText}</div>
+        <div>{this.state.errorText} </div>
 
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type="email"
-            name="email"
-            placeholder="Your email"
-            value={this.state.email}
-            onChange={this.handleChange}
-          />
-
-          <input
-            type="password"
-            name="password"
-            placeholder="Your password"
-            value={this.state.password}
-            onChange={this.handleChange}
-          />
-
-          <div>
-            <button type="submit">Login</button>
+        <form onSubmit={this.handleSubmit} className="auth-form-wrapper">
+          <div className="form-group">
+            <FontAwesomeIcon icon="envelope" />
+            <input
+              type="email"
+              name="email"
+              placeholder="Your email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
           </div>
+
+          <div className="form-group">
+            <FontAwesomeIcon icon="lock" />
+            <input
+              type="password"
+              name="password"
+              placeholder="Your password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <button className="btn" type="submit">
+            Login
+          </button>
         </form>
       </div>
     );
